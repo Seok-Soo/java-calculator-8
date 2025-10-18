be87b1cba4d6cd6aabc8a3f2d219ddbc46de1ebc
Seok_Soo <whdtn7855@naver.com>
2025-10-18 15:42:51 +0900

    커스텀 구분자 에러 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,12 +1,8 @@
 package calculator;
 import camp.nextstep.edu.missionutils.Console;
-
+import java.util.regex.Pattern;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        // 문자열을 숫자로 변환해서 더하는 함수
-        // 새로운 구분자를 만드는 함수
-        // 사용자가 잘못된 입력을 했을 때의 예의값 처리
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
@@ -23,17 +19,20 @@ public class Application {
         String numbers = input;
 
         if (input.startsWith("//")) {
-            int index = input.indexOf("\n");
+            int index = input.indexOf("\\n");
             if (index == -1) {
                 throw new IllegalArgumentException("[ERROR] 커스텀 구분자 형식이 잘못되었습니다. (예: //;\\n1;2;3)");
             }
-            delimiter = input.substring(2, index);
-            numbers = input.substring(index + 1);
+
+            String customDelimiter = input.substring(2, index);
+            delimiter = delimiter + "|" + Pattern.quote(customDelimiter); // 정규식 특수문자 처리
+            numbers = input.substring(index + 2);
         }
 
         String[] tokens = numbers.split(delimiter);
         int sum = 0;
         for (String token : tokens) {
+            if (token.isEmpty()) continue; // 빈 값 방지
             sum += Integer.parseInt(token);
         }
         return sum;
